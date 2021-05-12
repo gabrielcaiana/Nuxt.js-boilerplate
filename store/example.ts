487f71457a33154d73b5efba46cf711c42cdbb47
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-05-12 20:43:41 -0300

    referenciando o axios

--- store/example.ts
+++ store/example.ts
@@ -1,4 +1,5 @@
 import { Module, VuexModule} from 'vuex-module-decorators'
+import {$axios} from '@/utils/nuxt-instance'
 
 @Module({ name: 'example', stateFactory: true, namespaced: true })
 export default class Example extends VuexModule {
