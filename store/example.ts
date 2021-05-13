15cb159e11a6c305e36a113084f6f8aef4555374
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-05-12 23:15:33 -0300

    examplo de getters, mutations e actions

--- store/example.ts
+++ store/example.ts
@@ -1,7 +1,21 @@
-import { Module, VuexModule} from 'vuex-module-decorators'
+import { Module, VuexModule, Mutation, Action} from 'vuex-module-decorators'
 import {$axios} from '@/utils/nuxt-instance'
 
 @Module({ name: 'example', stateFactory: true, namespaced: true })
 export default class Example extends VuexModule {
-  example  = []
+  private example  = 0
+
+  public get $example() {
+    return this.example
+  }
+
+  @Mutation
+  private INCREMENT(number: number) {
+    this.example += number
+  }
+
+  @Action
+  public increment() {
+    this.context.commit('INCREMENT', 5)
+  }
 }
